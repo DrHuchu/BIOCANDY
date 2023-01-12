bb8e55b0b73d90f046602acd114308e21c4e0780
MAGICTURTLE <21bjh21@gmail.com>
2023-01-12 19:12:56 +0900

    230112 첫 커밋
    에너미 추적 추가

---
+++ Source/BIOCANDY/Enemy.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AEnemy::AEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	sphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("Radius for Recognition"));
+	sphereComp->SetupAttachment(RootComponent);
+	sphereComp->SetSphereRadius(recognitionRadius);
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	sphereComp->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::Moving);
+	sphereComp->OnComponentEndOverlap.AddDynamic(this, &AEnemy::RecognitionOff);
+}
+
+// Called every frame
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	
+	//플레이어를 인식할 경우 = isRecognized가 true일 경우
+	if (isRecognized)
+	{
+		//direction을 플레이어 방향으로 설정
+		//direction = player->GetActorLocation() - GetActorLocation();
+		//direction을 노말라이즈
+		//direction.Normalize();
+		//direction 방향으로 moveSpeed의 속도로 이동
+		SetActorLocation(GetActorLocation() + direction * moveSpeed * DeltaTime);
+	}
+	else
+	{
+		return;
+	}
+}
+
+// Called to bind functionality to input
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AEnemy::Moving(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//오버랩된게 플레이어라면
+	//if (player != nullptr)
+	//{
+		//recogDelay만큼 시간이 흐른 뒤에 is Recognized를 true로 변경
+		FTimerHandle recogTimer;
+		GetWorld()->GetTimerManager().SetTimer(recogTimer, this, &AEnemy::Recognition, recogDelay, false);
+	//}
+}
+
+void AEnemy::Recognition()
+{
+	isRecognized = true;
+}
+
+void AEnemy::RecognitionOff(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	isRecognized = false;
+}
+
---
+++ Source/BIOCANDY/Enemy.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class BIOCANDY_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemy();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Settings")
+		class USphereComponent* sphereComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Settings")
+		float recognitionRadius = 400.0f;
+
+	FVector direction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Settings")
+		float moveSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Settings")
+		float recogDelay;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Settings")
+		bool isRecognized = false;
+
+
+	UFUNCTION()
+		void Moving(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void Recognition();
+	UFUNCTION()
+		void RecognitionOff(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+	//class APlayer_Jill* player;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
