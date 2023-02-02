80ee127321e7bd1c108b8e1466c46fb28a6678f3
won7527 <121008852+won7527@users.noreply.github.com>
2023-02-02 12:58:13 +0900

    에셋

---
+++ Source/BIOCANDY/Public/PlayerJillAnim.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerJillAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIOCANDY_API UPlayerJillAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
