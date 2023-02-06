501c8d348d11ecec681766e83e8226a026430855
Huchu <21bjh21@gmail.com>
2023-02-06 14:12:05 +0900

    230206 발표 전 머지

--- Source/BIOCANDY/Private/Pickable_Bullet.cpp
+++ Source/BIOCANDY/Private/Pickable_Bullet.cpp
@@ -3,11 +3,19 @@
 
 #include "Pickable_Bullet.h"
 
+#include "Player_Jill.h"
+
 void APickable_Bullet::InteractWithMe()
 {
 	if (!bIsOn)
 	{
-		//게임모드에 있는 탄약수 15 증가
+		player = Cast<APlayer_Jill>(GetWorld()->GetFirstPlayerController()->GetPawn());
+
+		if(player)
+		{
+			//플레이어에 있는 탄약수 15 증가
+			player->pistolCountBag =  player->pistolCountBag + 15;
+		}
 
 		Destroy();
 		bIsOn = true;
